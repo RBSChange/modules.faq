2888ebd31197f53d32e6aea503be5ff356af4077
Franck STAUFFER <franck.stauffer@rbs.fr>
2011-08-23 11:19:13 +0200

    New class injection comptability

--- lib/services/FaqService.class.php
+++ lib/services/FaqService.class.php
@@ -13,7 +13,7 @@ class faq_FaqService extends f_persistentdocument_DocumentService
 	{
 		if (self::$instance === null)
 		{
-			self::$instance = self::getServiceClassInstance(get_class());
+			self::$instance = new self();
 		}
 		return self::$instance;
 	}
